9dfbbe09b75a5a050db3ca52049a64ad3cc43fa9
jjustinmayo <jjustinmayo@gmail.com>
2024-08-09 14:45:48 -0400

    exits after 3 failed attempts

--- GuessingGame.py
+++ GuessingGame.py
@@ -4,5 +4,24 @@
     
 #except:
     #print("fail")
+
     
-    #Above is how you can test if importing a specific library is working 
+import random
+
+number = random.randint(0,10)
+
+lives = 3
+
+while lives > 0:
+    answer = input("enter number: ")
+    if(answer == number):
+        print("correct")
+    else: 
+        lives -= 1
+        print("wrong... you have", lives," more lives")
+
+else: 
+    print ("fuck off")
+    
+
+#ctrl-/ => comment everything
