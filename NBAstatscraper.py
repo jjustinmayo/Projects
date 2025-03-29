9d0d1eee3a62228b9a2415f98bef16e1b477411b
jjustinmayo <jjustinmayo@gmail.com>
2025-03-29 11:30:57 -0400

    another test

--- NBAstatscraper.py
+++
@@ -1,9 +0,0 @@
-
-from basketball_reference_scraper.teams import get_roster, get_team_stats  #instructions on how to use
-import pandas as pd
-import requests
-
-# API documentation: https://github.com/vishaalagartha/basketball_reference_scraper/blob/master/API.md
-
-#print(get_roster('TOR',2019))
-print(get_team_stats('TOR', 2019, data_format='TOTALS'))
