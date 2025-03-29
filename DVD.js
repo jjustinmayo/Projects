d3ccbde04668209943c3cd10d49c365bc3048653
jjustinmayo <jjustinmayo@gmail.com>
2025-03-29 11:27:26 -0400

    Added bball ref scraper

--- DVD.js
+++
@@ -1,8 +0,0 @@
-
-open();
-
-// 1. Create a dynamically fixed area based on the size of the screen
-// 2. create a box; make that box a "DVD jpeg"
-// 3. Make the box move in any random direction upon a refresh or load
-// 4. when the box hits the wall, it will go in the opposite diagonal it hit the wall
-// 5. if it hits a corner perfectly, it will push against 2 diagonals in the inverse direction (I think)
